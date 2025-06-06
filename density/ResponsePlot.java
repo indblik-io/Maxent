06a6e335ff54b17d9e1e33d33b1dded40be4c8f5
nldoc <jasa@posteo.de>
2025-06-06 14:16:17 +0200

    deactivate responsecurves png rendering

--- density/ResponsePlot.java
+++ density/ResponsePlot.java
@@ -56,10 +56,10 @@ public class ResponsePlot extends MyPlot {
     }	
 
     void makeplot(double[] x, double[] y, double[] stddev, boolean iscategorical, String var, String title, String ylabel, String outfile, double mmin, double mmax, Params params, boolean exponent, boolean writePlotData) throws IOException {
-	setSize(600,400);
-	setXLabel(var);
-	setYLabel(ylabel);
-	makeplot(x,y,stddev,!exponent,iscategorical,title);
+	// setSize(600,400);
+	// setXLabel(var);
+	// setYLabel(ylabel);
+	// makeplot(x,y,stddev,!exponent,iscategorical,title);
 	if (writePlotData) {
 	    PrintWriter plotDataOut = new PrintWriter(new FileOutputStream(outfile+".dat"));
 	    plotDataOut.println("variable,x,y");
@@ -67,31 +67,31 @@ public class ResponsePlot extends MyPlot {
 		plotDataOut.println(var + "," + x[i] + "," + y[i]);
 	    plotDataOut.close();
 	}
-	String filename = outfile + ".png";
-	String filename2 = outfile + "_thumb.png";
-	ImageIO.write(exportImage(), "png", new File(filename));
-	setSize(210, 140);
-	setTitle(var);
-	setXLabel("");
-	setYLabel("");
-	_topPadding = 5;
-	_bottomPadding = 0;
-	_rightPadding = 0;
-	_leftPadding = 0;
-	if (params.occurrenceProbability() && !exponent) setYRange(0,1);
-	if (params.cumulative() && !exponent) setYRange(0,100);
-	setTitleFont("Helvetica bold 12");
-	setLabelFont("Helvetica plain 9");
-	setSuperscriptFont("Helvetica plain 6");
+	// String filename = outfile + ".png";
+	// String filename2 = outfile + "_thumb.png";
+	// ImageIO.write(exportImage(), "png", new File(filename));
+	// setSize(210, 140);
+	// setTitle(var);
+	// setXLabel("");
+	// setYLabel("");
+	// _topPadding = 5;
+	// _bottomPadding = 0;
+	// _rightPadding = 0;
+	// _leftPadding = 0;
+	// if (params.occurrenceProbability() && !exponent) setYRange(0,1);
+	// if (params.cumulative() && !exponent) setYRange(0,100);
+	// setTitleFont("Helvetica bold 12");
+	// setLabelFont("Helvetica plain 9");
+	// setSuperscriptFont("Helvetica plain 6");
     
-	if (!iscategorical) {
-	    NumberFormat nf = NumberFormat.getNumberInstance(Locale.US);
-	    nf.setGroupingUsed(false);
-	    nf.setMaximumFractionDigits(3);
-	    addXTick(nf.format(mmin), mmin);
-	    addXTick(nf.format(mmax), mmax);
-	}
-	repaint();
-	ImageIO.write(exportImage(), "png", new File(filename2));
+	// if (!iscategorical) {
+	//     NumberFormat nf = NumberFormat.getNumberInstance(Locale.US);
+	//     nf.setGroupingUsed(false);
+	//     nf.setMaximumFractionDigits(3);
+	//     addXTick(nf.format(mmin), mmin);
+	//     addXTick(nf.format(mmax), mmax);
+	// }
+	// repaint();
+	// ImageIO.write(exportImage(), "png", new File(filename2));
     }
 }
